c4312fe449a37d86d8085fbe51f8bb3827599028
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-01-09 11:06:43 +0100

    project: add bin and hex files

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DIGITAL_IN1_Pin GPIO_PIN_4
+#define DIGITAL_IN1_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
