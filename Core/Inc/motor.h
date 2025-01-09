2fe1218816b494f2a59213fa46627e4a4171aa92
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-01-09 13:24:02 +0100

    project: short but important comment

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -20,6 +20,8 @@ typedef enum {
 	MOTOR_RIGHT = 0
 } MotorDirection_t;
 
+//frequency used in PWM is 10 kHz
+
 void Motor_Init(void); //first function to call when using this library
 void Motor_Percent_Control(const MotorDirection_t direction, const int8_t velocity); //sets motor in given direction and speed in range from 0 - 100
 void Motor_Stop(void); //motor doesnt receive any control signals
