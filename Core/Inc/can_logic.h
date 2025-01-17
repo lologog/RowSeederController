7261a2bc0875bcc12676d66e08f74506a833c107
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-01-17 08:10:57 +0100

    project: add can_logic files to handle main loop with flags logic

---
+++ Core/Inc/can_logic.h
@@ -0,0 +1,15 @@
+/*
+ * can_logic.h
+ *
+ *  Created on: Jan 17, 2025
+ *      Author: krzysztof.tomicki
+ */
+
+#ifndef INC_CAN_LOGIC_H_
+#define INC_CAN_LOGIC_H_
+
+//access to HAL
+#include "stm32l4xx_hal.h"
+#include "main.h"
+
+#endif /* INC_CAN_LOGIC_H_ */
---
+++ Core/Src/can_logic.c
@@ -0,0 +1,9 @@
+/*
+ * can_logic.c
+ *
+ *  Created on: Jan 17, 2025
+ *      Author: krzysztof.tomicki
+ */
+
+#include "can_logic.h"
+
